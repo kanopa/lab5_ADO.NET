892bc3c7861e0a140e4f6dc54737549d3f0d017d
Dima Konoplyanka <konoplyanka1999@gmail.com>
2019-06-10 01:11:20 +0300

    Add output

--- lab5_ADO.NET/lab5_ADO.NET/Program.cs
+++ lab5_ADO.NET/lab5_ADO.NET/Program.cs
@@ -12,22 +12,37 @@ namespace lab5_ADO.NET
     {
         static void Main(string[] args)
         {
-            string connect = "Server=127.0.0.1;Port=5432;Database=test;User Id=postgres;Password = 7654321d;";
+            string connect = "Server=127.0.0.1;Port=5432;Database=test;User Id=postgres;Password = 7654321dpostgresql;";
             NpgsqlConnection connection = new NpgsqlConnection(connect);
             connection.Open();
             string sql = "select* from InfoChemicals";
+            string sql2 = "select* from ContractChemicals";
             NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(sql, connection);
             DataSet dataSet = new DataSet();
-            DataTable dt = new DataTable();
+            DataTable dataTable = new DataTable();
+            DataTable dataTable2 = new DataTable();
             
             adapter.Fill(dataSet, "InfoChemicals");
-            dt = dataSet.Tables["InfoChemicals"];
-            foreach (DataRow dataRow in dt.Rows)
+            dataTable = dataSet.Tables["InfoChemicals"];
+            Console.WriteLine("Таблица InfoChemicals");
+            Console.WriteLine(new string('-', 100));
+            foreach (DataRow dataRow in dataTable.Rows)
             {
-                Console.WriteLine("{0} {1}",dataRow["NameChemicals"].ToString(),
-                    dataRow["TypeChemical"].ToString());
+                Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}",dataRow["NameChemicals"].ToString(),
+                    dataRow["TypeChemical"].ToString(), dataRow["opportunities"].ToString(), dataRow["ChemicalComposition"].ToString(),
+                    dataRow["ManufacturingCompany"].ToString(),dataRow["CountChemicals"].ToString(),dataRow["price"].ToString());
             }
-
+            Console.WriteLine(new string('-', 100));
+            NpgsqlDataAdapter adapter2 = new NpgsqlDataAdapter(sql2, connection);
+            adapter2.Fill(dataSet, "ContractChemicals");
+            dataTable2 = dataSet.Tables["ContractChemicals"];
+            Console.WriteLine("\nТаблица ContractChemicals");
+            Console.WriteLine(new string('-', 100));
+            foreach (DataRow dataRow in dataTable2.Rows)
+            {
+                Console.WriteLine("{0} {1} {2}", dataRow["NumberContract"].ToString(),dataRow["Enterprise"].ToString(),dataRow["CountProcured"].ToString());
+            }
+            Console.WriteLine(new string('-', 100));
         }
     }
 }/*create table InfoChemicals(
